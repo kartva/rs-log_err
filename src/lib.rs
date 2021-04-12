bdccbc2990bc22629bd4d47cc61d746683eab1ab
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-04-12 21:06:01 +0530

    Rework testing infra

--- src/lib.rs
+++ src/lib.rs
@@ -31,19 +31,28 @@ impl<T, E: Debug> LogErrResult<T, E> for Result<T, E> {
 #[cfg(test)]
 mod test {
     use super::*;
+    
     use simplelog::SimpleLogger;
 
-    #[test]
-    #[should_panic(expected = "a wild error has appeared!")]
-    fn test_log_unwrap () {
+    static mut LOGGER_SET_UP: bool = false;
+
+    fn setup_logger () {
         SimpleLogger::init(simplelog::LevelFilter::Debug, simplelog::Config::default()).unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "called `Result::unwrap()` on an `Err` value: \"a wild error has appeared!\"")]
+    fn test_log_unwrap () {
+        unsafe {if !LOGGER_SET_UP {setup_logger(); LOGGER_SET_UP = true;}}
+
         Result::<(), &str>::Err("a wild error has appeared!").log_unwrap();
     }
 
     #[test]
-    #[should_panic(expected = "a wild error has appeared!")]
+    #[should_panic(expected = "A wild error SHOULD appear: \"a wild error has appeared!\"")]
     fn test_log_expect () {
-        SimpleLogger::init(simplelog::LevelFilter::Debug, simplelog::Config::default()).unwrap();
-        Result::<(), &str>::Err("a wild error has appeared!").log_expect("A wild error SHOULD appear");
+        unsafe {if !LOGGER_SET_UP {setup_logger(); LOGGER_SET_UP = true;}}
+
+        (Result::<(), String>::Err("a wild error has appeared!".to_string())).log_expect("A wild error SHOULD appear");
     }
 }
