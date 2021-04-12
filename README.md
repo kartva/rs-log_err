5c899e212625d6c810eb86b4a09da45e7b503b52
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-04-12 20:44:57 +0530

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# Log_Err
+A small extension to the [log](https://crates.io/crates/log) crate, which provides two methods for `core::result::Result<T, E>` :
+
+`log_except` and `log_unwrap`, which invoke the `log::error!` macro (in case of `Result::Err`) in _addition_ to unwrapping/expecting the `Result`.
+
+Shorthand for:
+
+```rust
+something().map_err(|e| error!("{}: {:?}", msg, e)).expect(msg)
+```
+
+Example:
+
+```rust
+let mut file = File::create("foo.txt").log_expect("Error creating file");
+```
+```
+# Will output on your logger and on the main program as well:
+
+# Or however your logger formats messages
+
+[ERROR] Error creating file: Os { code: 2, kind: NotFound, message: "No such file or directory" }
+
+# Main program panic'ing with same message
+
+thread 'main' panicked at 'Error creating file: Os { code: 2, kind: NotFound, message: "No such file or directory" }', test.rs:4:48
+```
---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use log::error;
+use std::fmt::Debug;
+
+pub trait LogErrResult<T, E: Debug> {
+    fn log_unwrap (self) -> T;
+    fn log_expect (self, msg: &str) -> T;
+    
+//   fn is_ok(&self) -> bool;
+//   fn is_err(&self) -> bool;
+}
+
+impl<T, E: Debug> LogErrResult<T, E> for Result<T, E> {
+    fn log_unwrap (self) -> T {
+        self.map_err(|e| error!("called `Result::unwrap()` on an `Err` value: {:?}", e)).unwrap()
+    }
+
+    fn log_expect (self, msg: &str) -> T {
+        self.map_err(|e| error!("{}: {:?}", msg, e)).expect(msg)
+    }
+/*
+    fn is_ok(&self) -> bool {
+        matches!(*self, Ok(_))
+    }
+
+    fn is_err(&self) -> bool {
+        !self.is_ok()
+    }
+*/
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use simplelog::SimpleLogger;
+
+    #[test]
+    #[should_panic(expected = "a wild error has appeared!")]
+    fn test_log_unwrap () {
+        SimpleLogger::init(simplelog::LevelFilter::Debug, simplelog::Config::default()).unwrap();
+        Result::<(), &str>::Err("a wild error has appeared!").log_unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "a wild error has appeared!")]
+    fn test_log_expect () {
+        SimpleLogger::init(simplelog::LevelFilter::Debug, simplelog::Config::default()).unwrap();
+        Result::<(), &str>::Err("a wild error has appeared!").log_expect("A wild error SHOULD appear");
+    }
+}
