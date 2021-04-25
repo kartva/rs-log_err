1f7825619d874708d7d99baaf2fe35e2b05230b6
Kartavya Vashishtha <kartavya0703@gmail.com>
2021-04-25 16:16:17 +0530

    Add Option trait for log_unwrap, log_expect
    Update doc tests to pass

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,25 @@
 /*!
 # Log_Err
-A small extension to the [log](https://crates.io/crates/log) crate, which provides two methods for `core::result::Result<T, E>` :
+A small extension to the [log](https://crates.io/crates/log) crate, which provides two methods for `core::result::Result<T, E>` and `core:::
 
 `log_except` and `log_unwrap`, which invoke the `log::error!` macro (in case of `Result::Err`) in _addition_ to unwrapping/expecting the `Result`.
 
 Shorthand for:
 
-```rust
+```should_panic
+# use log::error;
+# fn something() -> Result<(), &'static str> {Err("there was some problem")}
+# let msg = "Some message";
 something().map_err(|e| error!("{}: {:?}", msg, e)).expect(msg)
 ```
 
 Example:
 
-```rust
-let mut file = File::create("foo.txt").log_expect("Error creating file");
+```should_panic
+# use std::fs::File;
+# use log::error;
+# use log_err::LogErrResult;
+let mut file = File::open("foo.txt").log_expect("Error creating file");
 ```
 ```text
 # Error will be logged with the error! macro
@@ -30,9 +36,42 @@ use std::fmt::Debug;
 pub trait LogErrResult<T, E: Debug> {
     fn log_unwrap (self) -> T;
     fn log_expect (self, msg: &str) -> T;
-    
-//   fn is_ok(&self) -> bool;
-//   fn is_err(&self) -> bool;
+}
+
+pub trait LogErrOption<T> {
+    fn log_unwrap (self) -> T;
+    fn log_expect (self, msg: &str) -> T;
+}
+
+impl<T> LogErrOption<T> for Option<T> {
+
+    /**
+    `unwrap`s the `Option`, and outputs error message (in exact same style as `unwrap`) through `error!` as well.
+    */
+
+    fn log_unwrap (self) -> T {
+        match self {
+            Some (n) => n,
+            None => {
+                error!("called `Option::unwrap()` on a `None` value");
+                self.unwrap()
+            }
+        }
+    }
+
+    /**
+    `expect`s the `Option`, and outputs error message (in exact same style as `expect`) through `error!` as well.
+    */
+
+    fn log_expect (self, msg: &str) -> T {
+        match self {
+            Some (n) => n,
+            None => {
+                error!("{}", msg);
+                self.expect(msg)
+            },
+        }
+    }
 }
 
 impl<T, E: Debug> LogErrResult<T, E> for Result<T, E> {
@@ -52,15 +91,6 @@ impl<T, E: Debug> LogErrResult<T, E> for Result<T, E> {
     fn log_expect (self, msg: &str) -> T {
         self.map_err(|e| {error!("{}: {:?}", msg, e); e}).expect(msg)
     }
-/*
-    fn is_ok(&self) -> bool {
-        matches!(*self, Ok(_))
-    }
-
-    fn is_err(&self) -> bool {
-        !self.is_ok()
-    }
-*/
 }
 
 #[cfg(test)]
@@ -84,10 +114,18 @@ mod test {
     }
 
     #[test]
-    #[should_panic(expected = "A wild error SHOULD appear: \"a wild error has appeared!\"")]
-    fn test_log_expect () {
+    #[should_panic(expected = "A wild error SHOULD appear")]
+    fn test_option_log_expect () {
         unsafe {if !LOGGER_SET_UP {setup_logger(); LOGGER_SET_UP = true;}}
 
-        (Result::<(), String>::Err("a wild error has appeared!".to_string())).log_expect("A wild error SHOULD appear");
+        Option::<()>::None.log_expect("A wild error SHOULD appear");
+    }
+
+    #[test]
+    #[should_panic(expected = "called `Option::unwrap()` on a `None` value")]
+    fn test_option_log_unwrap () {
+        unsafe {if !LOGGER_SET_UP {setup_logger(); LOGGER_SET_UP = true;}}
+
+        Option::<()>::None.log_unwrap();
     }
 }
